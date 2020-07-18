0ae4c8c1e05838b7f34363fd53fdeffa58a3e741
Kevin Lee <kevin.code@kevinlee.io>
2020-07-18 13:48:13 +1000

    Change: syntax highlight theme

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -19,6 +19,10 @@ const websiteConfig = {
   organizationName: 'otter275', // Usually your GitHub org/user name.
   projectName: 'math', // Usually your repo name.
   themeConfig: {
+    prism: {
+      theme: require('prism-react-renderer/themes/nightOwl'),
+      darkTheme: require('prism-react-renderer/themes/nightOwl'),
+    },
     navbar: {
       title: 'Math U',
       logo: {
