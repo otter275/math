31d3b3f040dfdc26c7020895be9e28bdd9ef1944
Matt <ayanami275@gmail.com>
2020-07-19 15:53:56 -0600

    blog commit 01

--- website/sidebars.js
+++ website/sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Math U': ['math-u'],
-    '통계학': ['00-statistics/00', '00-statistics/01'],
+    '통계학': ['00-statistics/00', '00-statistics/01', '00-statistics/02'],
     '확률론': ['01-probability-theory/00'],
     '해석학': ['02-analysis/00'],
     '현대 대수학': ['03-modern-algebra/00'],
@@ -9,6 +9,6 @@ module.exports = {
     '집합론': ['05-set-theory/00'],
     '수치해석': ['06-numerical-method/00'],
     '미분방정식': ['07-differential-equation/00'],
-    '응용수학': ['08-applied-mathematics/00'],
+    '수학스터디 미팅': ['08-math-study-meeting/00'],
   },
 };
